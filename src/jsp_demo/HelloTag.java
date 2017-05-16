43d51c51831a830da8ddbe31b99119e2d4191c33
Jinze0830 <jinze1990@gmail.com>
2017-05-16 14:35:46 -0400

    changed first to 1st

--- src/jsp_demo/HelloTag.java
+++ src/jsp_demo/HelloTag.java
@@ -8,8 +8,6 @@ import java.io.*;
 public class HelloTag extends SimpleTagSupport {
    public void doTag() throws JspException, IOException {
       JspWriter out = getJspContext().getOut();
-      out.println("Hello First Custom Tag!");
-      
-      
+      out.println("Hello 1st Custom Tag!");      
    }
 }
